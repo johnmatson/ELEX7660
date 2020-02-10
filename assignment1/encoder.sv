bb4ca5dc9d1b54c03e274f7bfdd9b4158699ad7e
John Matson <john@matsonengineering.com>
2020-02-10 00:24:02 -0800

    assignment 1 question 1 v0.1

---
+++ assignment1/encoder.sv
@@ -0,0 +1,21 @@
+module encoder (output logic [1:0] y,
+                output logic valid,
+                input logic [3:0] a);
+
+    always_comb begin
+        if (a) begin
+            valid = 'b1;
+            if (a[3])
+                y = 'b11;
+            else if (a[2])
+                y = 'b10;
+            else if (a[1])
+                y = 'b01;
+            else if (a[0])
+                y = 'b00;
+        end
+        else
+            valid = 'b0;
+    end
+
+endmodule
---
+++ assignment1/shiftReg.sv
@@ -0,0 +1,6 @@
+module shiftReg #(parameter N = 8) (output logic [N-1:0] q,
+                                    input logic [N-1:0] a,
+                                    input logic [1:0] s
+                                    input logic shiftIn, clk, reset_n);
+
+endmodule
