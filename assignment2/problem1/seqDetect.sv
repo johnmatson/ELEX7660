851ccac4bb24ed76c787a14b4d7c77935ae5374c
John Matson <john@matsonengineering.com>
2020-02-21 15:51:42 -0800

    assignment 2.1 v0.2

--- assignment2/problem1/seqDetect.sv
+++ assignment2/problem1/seqDetect.sv
@@ -1,5 +1,25 @@
-module seqDetect (  output logic valid,
-                    input logic a, input logic [3:0] seq,
-                    input logic clk, reset_n);
+module seqDetect (  output logic valid, // output valid signal for correct sequence
+                    input logic a, // serial sequence data input
+                    input logic [3:0] seq, // correct signal for comparison
+                    input logic clk, reset_n); 
+
+    logic [3:0] A; // concatenated sequence from input signal a
+
+    always_ff @ (posedge clk, negedge reset_n) begin
+
+        // check for asychronous resest
+        if (~reset_n) begin
+            valid <= 0;
+        end
+
+        // shift a bit in and compare sequence
+        else begin
+            A <= {a, A[2:0]};
+            if (A==a)
+                valid <= 1;
+            else
+                valid <= 0;
+        end
+    end
 
 endmodule
---
+++ assignment2/problem1/seqDetect_tb.sv
@@ -0,0 +1,30 @@
+module seqDetect_tb;
+
+    logic valid; // output valid signal for correct sequence
+    logic a; // serial sequence data input
+    logic [3:0] seq = 0100; // correct signal for comparison
+    logic clk, reset_n;
+
+    int i;
+
+    initial begin
+        reset_n = 0;
+        clk = 0;
+        a = 0;
+        i = 0;
+    
+        #5ns reset_n = 1;
+    forever begin
+        #5ns clk = ~clk;
+        a = seq[i%4]
+        i++;
+    end
+    end
+
+    initial begin
+        #50ns reset_n = 0;
+        #70ns reset_n = 1;
+        #100ns $stop;
+    end
+
+endmodule
