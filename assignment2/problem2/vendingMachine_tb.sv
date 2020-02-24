18c6cbefb0602887a5ab768408c721aceabff887
John Matson <47843491+johnmatson@users.noreply.github.com>
2020-02-23 16:20:30 -0800

    assignment 2.2 v1.2

--- assignment2/problem2/vendingMachine_tb.sv
+++ assignment2/problem2/vendingMachine_tb.sv
@@ -11,6 +11,7 @@ module vendingMachine_tb;
         dime = 0;
         quarter = 0;
         reset_n = 0;
+        clk = 0;
 
         #5ns reset_n = 1;
 
@@ -19,22 +20,25 @@ module vendingMachine_tb;
             dime = 0;
             quarter = 0;
             clk = ~clk;
+            #5ns clk = ~clk;
 
             #5ns nickel = 0;
             dime = 1;
             quarter = 0;
             clk = ~clk;
+            #5ns clk = ~clk;
 
             #5ns nickel = 0;
             dime = 0;
             quarter = 1;
             clk = ~clk;
+            #5ns clk = ~clk;
         end
     end
     
     initial begin
-        #50ns reset_n = 0;
-        #5ns reset_n = 1;
-        #45ns $stop;
+        //#50ns reset_n = 0;
+        //#5ns reset_n = 1;
+        #200ns $stop;
     end
 endmodule
