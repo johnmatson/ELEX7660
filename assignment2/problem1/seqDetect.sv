1f85188318f8a4aec63d220af5d9f83f4d1a5ed8
John Matson <john@matsonengineering.com>
2020-02-23 16:57:50 -0800

    assignment 2.1 v1.3

--- assignment2/problem1/seqDetect.sv
+++ assignment2/problem1/seqDetect.sv
@@ -4,23 +4,26 @@ module seqDetect (  output logic valid, // output valid signal for correct seque
                     input logic clk, reset_n); 
 
     logic [3:0] A; // concatenated sequence from input signal a
+    logic [3:0] A_comb;
 
     always_ff @ (posedge clk, negedge reset_n) begin
 
         // check for asychronous resest
         if (~reset_n) begin
-            valid <= 0;
             A <= 0;
         end
 
         // shift a bit in and compare sequence
         else begin
             A <= {A[2:0], a};
-            if (A==seq)
-                valid <= 1;
-            else
-                valid <= 0;
         end
+    end
+
+    always_comb begin
+        if (A==seq)
+            valid = 1;
+        else
+            valid = 0;
     end
 
 endmodule
